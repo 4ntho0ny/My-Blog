fd374a2640eced81b8db06b331c909ffed7b5b60
Anthony <114087139+4ntho0ny@users.noreply.github.com>
2024-05-03 19:49:41 -0400

    To build all-posts page, post details page and your styles

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="posts-detail-page") # /posts/my-first-post
+    path("posts/<slug:slug>", views.post_detail, 
+         name="posts-details-page") # /posts/my-first-post
 ]
